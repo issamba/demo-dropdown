fb91a549bd7072003d6df2bed8182da60eea672f
issam_ba <issambenamara92@gmail.com>
2023-02-25 20:41:28 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {City} from "./Model/City";
+import {Country} from "./Model/Country";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demo-dropdown';
+export class AppComponent implements OnInit {
+
+  cities: City [] = [];
+  selectedCity: City = {code :"", label:""};
+  numbers: number[] = [5, 6, 45, 39, 33];
+  selectedNumber: number = 6;
+  countries: Country[] = [];
+  selectedCountry: Country = {code : "USA", fullName: "United states of America "};
+
+  ngOnInit(): void {
+    this.fillCities();
+    this.fillCountries();
+  }
+
+  private fillCities() {
+    this.cities.push({code: "NYK", label: "New York"},
+      {code: "PH", label: "Philadelphia"},
+      {code: "CH", label: "Chicago"},
+      {code: "LA", label: "Los Angles"},)
+  }
+
+  private fillCountries() {
+    this.countries.push({code: "TN", fullName: "Tunisia"},
+      {code: "DZ", fullName: "Algeria"},
+      {code: "USA", fullName: "United states of America "},
+      {code: "Fr", fullName: "France"},)
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {DropdownModule} from 'primeng/dropdown';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -10,7 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    DropdownModule,
+    FormsModule,
+
+    NgbModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
